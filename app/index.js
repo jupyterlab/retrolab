89c4c9529b951b08a02df4853b2d444c1ea5d159
Jason Weill <jweill@amazon.com>
2021-11-08 10:49:06 -0800

    Adds toc-extension to package.json

--- app/index.js
+++ app/index.js
@@ -180,7 +180,8 @@ async function main() {
             '@jupyterlab/tooltip-extension:manager',
             '@jupyterlab/tooltip-extension:notebooks'
           ].includes(id)
-        )
+        ),
+        require('@jupyterlab/toc-extension')
       ]);
       break;
     }
